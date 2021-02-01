d708ad9fefc3588ce8b99305c6aae244687202b3
Vivek Savjani <viveknsavjani@gmail.com>
2021-02-01 17:59:52 +0000

    Add files via upload

---
+++ Methods.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Astarfrompsuedo
+{
+    class Node
+    {
+        public int f { get; set; }
+        public int g { get; set; }
+        public int h { get; set; }
+        public List<Node> neighbours { get; set; }
+    }
+
+    class methods
+    {
+
+        static Node astar(Node startnode)
+        {
+            List<Node> openlist = new List<Node>();
+            openlist.Add(startnode);
+            List<Node> closedlist = new List<Node>();
+
+
+            Node currentnode = new Node();
+            Node destination = new Node();
+            Node nextnode = new Node();
+            while (currentnode != destination)
+            {
+                nextnode = openlist[1];
+                foreach (var node in openlist)
+                {
+                    if (node.f < nextnode.f) { nextnode = node; }
+                }
+
+                closedlist.Add(currentnode);
+
+                foreach (var neighbour in currentnode.neighbours) {
+                    if (neighbour.g < currentnode.g && closedlist.Contains(neighbour)) { }
+                }
+
+
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Astarfrompsuedo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            
+        }
+    }
+}
